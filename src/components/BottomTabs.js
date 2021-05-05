ebded9b6fe089b68d23d002236e3d15c7ac3c1e8
A7abhilash <a7abhilash@gmail.com>
2021-05-05 08:20:39 +0530

    updated bottom tabs

--- src/components/BottomTabs.js
+++ src/components/BottomTabs.js
@@ -1,36 +1,29 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Button} from 'react-native-paper';
 import {globalColors} from '../styles/styles';
 
 export default function BottomTabs({findBy, setFindBy, FIND_BY}) {
-  return (
-    <View style={styles.container}>
-      <Button
-        mode="contained"
-        style={styles.bottomBtns}
-        color={globalColors.Secondary}
-        onPress={() => setFindBy(FIND_BY.PIN)}
-        disabled={findBy === FIND_BY.PIN}>
-        Find by pin
-      </Button>
-      <Button
-        mode="contained"
-        style={styles.bottomBtns}
-        color={globalColors.Secondary}
-        onPress={() => setFindBy(FIND_BY.DISTRICT)}
-        disabled={findBy === FIND_BY.DISTRICT}>
-        Find by district
-      </Button>
-    </View>
+  return findBy === FIND_BY.PIN ? (
+    <Button
+      mode="contained"
+      style={styles.bottomBtns}
+      color={globalColors.Gray}
+      onPress={() => setFindBy(FIND_BY.DISTRICT)}>
+      Find by district
+    </Button>
+  ) : (
+    <Button
+      mode="contained"
+      style={styles.bottomBtns}
+      color={globalColors.Gray}
+      onPress={() => setFindBy(FIND_BY.PIN)}>
+      Find by pin code
+    </Button>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
   bottomBtns: {
     justifyContent: 'center',
   },
